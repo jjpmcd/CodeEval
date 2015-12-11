8d138f581df5b73b3add6da63f3415f843a45d91
James McDonnell <jjpmcd@gmail.com>
2015-12-11 23:01:04 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/BitPositions/BitPositions.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitPositions.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (null == line) continue;
+                    // do something with line
+                }
+        }
+    }
+}
