923a5a484ac2084dc4f835be05b9f760195a37c7
James McDonnell <jjpmcd@gmail.com>
2015-12-12 18:26:29 +0000

    Implemented solution.

---
+++ Easy/SumOfIntegersFromFile/SumOfIntegersFromFile.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace SumOfIntegersFromFile.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var total = 0;
+            using (var reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (null == line) continue;
+                    total += int.Parse(line);
+                }
+            Console.WriteLine(total);
+        }
+    }
+}
