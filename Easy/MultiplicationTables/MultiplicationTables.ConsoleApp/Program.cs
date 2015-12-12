0904e52ae9e7a8f11f468b6b851e37240027affa
James McDonnell <jjpmcd@gmail.com>
2015-12-12 17:24:48 +0000

    Implemented and tested solution.

--- Easy/MultiplicationTables/MultiplicationTables.ConsoleApp/Program.cs
+++ Easy/MultiplicationTables/MultiplicationTables.ConsoleApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultiplicationTables.ConsoleApp
 {
@@ -10,6 +6,31 @@ namespace MultiplicationTables.ConsoleApp
     {
         static void Main(string[] args)
         {
+            for (var i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(MultiplePrinter.PrintRow(i));
+            }
+        }
+    }
+
+    public static class MultiplePrinter
+    {
+        public static string PrintRow(int input)
+        {
+            const int max = 12;
+
+            var count = 1;
+            var current = input + input;
+            var output = input.ToString();
+
+            while (count < max)
+            {
+                output += string.Format("{0,4}", current);
+                current += input;
+                count++;
+            }
+
+            return output;
         }
     }
 }
--- Easy/MultiplicationTables/MultiplicationTables.UnitTests/UnitTests.cs
+++ Easy/MultiplicationTables/MultiplicationTables.UnitTests/UnitTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplicationTables.ConsoleApp;
 
 namespace MultiplicationTables.UnitTests
 {
@@ -7,8 +7,36 @@ namespace MultiplicationTables.UnitTests
     public class UnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstRow()
         {
+            int input = 1;
+            string expected = "1   2   3   4   5   6   7   8   9  10  11  12";
+
+            string result = MultiplePrinter.PrintRow(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SecondRow()
+        {
+            int input = 2;
+            string expected = "2   4   6   8  10  12  14  16  18  20  22  24";
+
+            string result = MultiplePrinter.PrintRow(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ThirdRow()
+        {
+            int input = 3;
+            string expected = "3   6   9  12  15  18  21  24  27  30  33  36";
+
+            string result = MultiplePrinter.PrintRow(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
