c70b3ff0331882891108487239b261a7f02d5ef5
James McDonnell <jjpmcd@gmail.com>
2015-12-13 19:29:37 +0000

    Implemented solution.

---
+++ Easy/FileSize/FileSize.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace FileSize.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(new FileInfo(args[0]).Length);
+        }
+    }
+}
