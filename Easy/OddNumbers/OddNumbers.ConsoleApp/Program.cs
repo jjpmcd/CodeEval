2ba60e1199801f43c4ec5e26942bcf19b5f4b627
James McDonnell <jjpmcd@gmail.com>
2015-12-13 19:20:47 +0000

    Implemented solution.

---
+++ Easy/OddNumbers/OddNumbers.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OddNumbers.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (var i = 1; i < 100; i += 2) { Console.WriteLine(i); }
+        }
+    }
+}
