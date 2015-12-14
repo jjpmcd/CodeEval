8e649fa190c7f83f4e653e63e21c0c84e120fd45
James McDonnell <jjpmcd@gmail.com>
2015-12-14 12:32:49 +0000

    Refactored GetIntersectingSet method to use for loops instead of LINQ. Reduced run time by 19ms.

--- Easy/SetIntersection/SetIntersection.ConsoleApp/Program.cs
+++ Easy/SetIntersection/SetIntersection.ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace SetIntersection.ConsoleApp
 {
@@ -23,10 +22,42 @@ namespace SetIntersection.ConsoleApp
         public static string GetIntersectingSet(string input)
         {
             var splitInput = input.Split(';');
-            var firstSet = splitInput[0].Split(',');
-            var secondSet = splitInput[1].Split(',');
-            var intersectingSet = firstSet.Intersect(secondSet).ToArray();
-            return intersectingSet.Length == 0 ? "" : intersectingSet.Aggregate((workingString, nextElement) => workingString + ',' + nextElement);
+            var firstSplit = splitInput[0].Split(',');
+            var secondSplit = splitInput[1].Split(',');
+
+            var firstSet = new int[firstSplit.Length];
+            var secondSet = new int[secondSplit.Length];
+
+            for (var index = 0; index < firstSplit.Length; index++)
+            {
+                firstSet[index] = int.Parse(firstSplit[index]);
+            }
+
+            for (var index = 0; index < secondSplit.Length; index++)
+            {
+                secondSet[index] = int.Parse(secondSplit[index]);
+            }
+
+            var result = "";
+            var next = 0;
+            foreach (var element in firstSet)
+            {
+                for (var index = next; index < secondSet.Length; index++)
+                {
+                    if (element < secondSet[index])
+                    {
+                        next = index;
+                        break;
+                    }
+                    if (element != secondSet[index]) { continue; }
+                    if (result != "") { result += ','; }
+                    result += element;
+                    next = index + 1;
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
--- Easy/SetIntersection/SetIntersection.UnitTests/UnitTests.cs
+++ Easy/SetIntersection/SetIntersection.UnitTests/UnitTests.cs
@@ -39,5 +39,49 @@ namespace SetIntersection.UnitTests
 
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = "95,96,97,98,99,100,101,102,103;60,61,62,63,64,65,66";
+            string expected = "";
+
+            string result = SetComparer.GetIntersectingSet(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string input = "76,77,78,79,80,81,82,83,84,85;72,73,74,75,76,77,78,79,80,81,82,83,84,85";
+            string expected = "76,77,78,79,80,81,82,83,84,85";
+
+            string result = SetComparer.GetIntersectingSet(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string input = "92,93,94,95,96,97,98,99,100,101,102;94,95,96,97,98,99,100,101,102,103,104,105,106";
+            string expected = "94,95,96,97,98,99,100,101,102";
+
+            string result = SetComparer.GetIntersectingSet(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            string input = "20,25,36,45,89;25,36,77,88,98";
+            string expected = "25,36";
+
+            string result = SetComparer.GetIntersectingSet(input);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
