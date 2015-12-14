5d86fe1fdbdce0968c41a58c1bf35bbbb33cedf2
James McDonnell <jjpmcd@gmail.com>
2015-12-14 16:30:36 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/RightmostChar/RightmostChar.UnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightmostChar.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
