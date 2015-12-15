15e535ef260f6063d4ec54968dfea91206f9841e
James McDonnell <jjpmcd@gmail.com>
2015-12-15 11:48:38 +0000

    Implemented and tested IsHappy method.

--- Easy/HappyNumbers/HappyNumbers.ConsoleApp/Program.cs
+++ Easy/HappyNumbers/HappyNumbers.ConsoleApp/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HappyNumbers.ConsoleApp
 {
@@ -16,8 +13,34 @@ namespace HappyNumbers.ConsoleApp
                 {
                     var line = reader.ReadLine();
                     if (null == line) continue;
-                    // do something with line
+                    Console.WriteLine(HappinessChecker.IsHappy(line));
                 }
+        }
+    }
+
+    public static class HappinessChecker
+    {
+        public static string IsHappy(string input)
+        {
+            var result = int.Parse(input);
+            var previousResults = new List<int>();
+
+            while (result != 1)
+            {
+                var dividend = result;
+                byte divisor = 10;
+                result = 0;
+                while (dividend != 0)
+                {
+                    int remainder;
+                    dividend = Math.DivRem(dividend, divisor, out remainder);
+                    result += remainder * remainder;
+                }
+                if (previousResults.Contains(result)) { return "0"; }
+                previousResults.Add(result);
+            }
+
+            return "1";
         }
     }
 }
--- Easy/HappyNumbers/HappyNumbers.UnitTests/UnitTests.cs
+++ Easy/HappyNumbers/HappyNumbers.UnitTests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HappyNumbers.ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HappyNumbers.UnitTests
@@ -7,8 +8,36 @@ namespace HappyNumbers.UnitTests
     public class UnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Number1IsAHappyNumber()
         {
+            string input = "1";
+            string expected = "1";
+
+            string result = HappinessChecker.IsHappy(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Number7IsAHappyNumber()
+        {
+            string input = "7";
+            string expected = "1";
+
+            string result = HappinessChecker.IsHappy(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Number22IsNotAHappyNumber()
+        {
+            string input = "22";
+            string expected = "0";
+
+            string result = HappinessChecker.IsHappy(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
