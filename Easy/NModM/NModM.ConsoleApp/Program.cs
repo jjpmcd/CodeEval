e69938c4a4099cde885a7b431136f8de7e0b729b
James McDonnell <jjpmcd@gmail.com>
2015-12-16 17:02:17 +0000

    Implemented and tested GetRemainder method.

--- Easy/NModM/NModM.ConsoleApp/Program.cs
+++ Easy/NModM/NModM.ConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace NModM.ConsoleApp
 {
@@ -10,6 +7,27 @@ namespace NModM.ConsoleApp
     {
         private static void Main(string[] args)
         {
+            using (var reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (null == line) continue;
+                    Console.WriteLine(Calculator.GetRemainder(line));
+                }
+        }
+    }
+
+    public static class Calculator
+    {
+        public static int GetRemainder(string input)
+        {
+            var splitInput = input.Split(',');
+            var dividend = int.Parse(splitInput[0]);
+            var divisor = int.Parse(splitInput[1]);
+
+            while (dividend >= divisor) { dividend -= divisor; }
+
+            return dividend;
         }
     }
 }
--- Easy/NModM/NModM.UnitTests/UnitTests.cs
+++ Easy/NModM/NModM.UnitTests/UnitTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NModM.ConsoleApp;
 
 namespace NModM.UnitTests
 {
@@ -9,6 +9,34 @@ namespace NModM.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "20,6";
+            int expected = 2;
+
+            int result = Calculator.GetRemainder(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "2,3";
+            int expected = 2;
+
+            int result = Calculator.GetRemainder(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "10,3";
+            int expected = 1;
+
+            int result = Calculator.GetRemainder(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
