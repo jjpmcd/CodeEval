46bd9da5a6c532dba9f177fce809024ad9e74122
James McDonnell <jjpmcd@gmail.com>
2015-12-17 15:50:30 +0000

    Implemented and tested HexToDecimal method.

--- Easy/HexToDecimal/HexToDecimal.ConsoleApp/Program.cs
+++ Easy/HexToDecimal/HexToDecimal.ConsoleApp/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HexToDecimal.ConsoleApp
 {
@@ -16,8 +12,66 @@ namespace HexToDecimal.ConsoleApp
                 {
                     var line = reader.ReadLine();
                     if (null == line) continue;
-                    // do something with line
+                    Console.WriteLine(BaseConverter.HexToDecimal(line));
                 }
+        }
+    }
+
+    public static class BaseConverter
+    {
+        public static int HexToDecimal(string input)
+        {
+            int result = HexDigitToDecimal(input[input.Length - 1]);
+            if (input.Length == 1) { return result; }
+            var factor = 16;
+            for (var index = input.Length - 2; index >= 0; index--)
+            {
+                result += HexDigitToDecimal(input[index]) * factor;
+                if (index == 0) break;
+                factor *= 16;
+            }
+            return result;
+        }
+
+        private static byte HexDigitToDecimal(char digit)
+        {
+            switch (digit)
+            {
+                case '0':
+                    return 0;
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                case 'a':
+                    return 10;
+                case 'b':
+                    return 11;
+                case 'c':
+                    return 12;
+                case 'd':
+                    return 13;
+                case 'e':
+                    return 14;
+                case 'f':
+                    return 15;
+                default:
+                    throw new ArgumentException();
+            }
         }
     }
 }
--- Easy/HexToDecimal/HexToDecimal.UnitTests/UnitTests.cs
+++ Easy/HexToDecimal/HexToDecimal.UnitTests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HexToDecimal.ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HexToDecimal.UnitTests
@@ -9,6 +10,23 @@ namespace HexToDecimal.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "9f";
+            int expected = 159;
+
+            int result = BaseConverter.HexToDecimal(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "11";
+            int expected = 17;
+
+            int result = BaseConverter.HexToDecimal(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
