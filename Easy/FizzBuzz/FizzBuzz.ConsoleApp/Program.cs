992bfd23a0d5ebc675616b98557e3e2d70562e4b
James McDonnell <jjpmcd@gmail.com>
2015-12-18 21:44:01 +0000

    Imported previous FizzBuzz solution from June 2014. Refactored it to run 46ms faster and use 266240 fewer bytes.

---
+++ Easy/FizzBuzz/FizzBuzz.ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(args[0]))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        Console.WriteLine(FizzBuzzer.GenerateSequence(line));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class FizzBuzzer
+    {
+        public static string GenerateSequence(string input)
+        {
+            var numerals = input.Split(' ');
+            var a = IntParseFast(numerals[0]);
+            var b = IntParseFast(numerals[1]);
+            var n = IntParseFast(numerals[2]);
+
+            var builder = new StringBuilder();
+            for (var index = 1; index < n + 1; index++)
+            {
+                var conversionPerformed = false;
+                if (index % a == 0)
+                {
+                    builder.Append('F');
+                    conversionPerformed = true;
+                }
+                if (index % b == 0)
+                {
+                    builder.Append('B');
+                    conversionPerformed = true;
+                }
+                if (!conversionPerformed)
+                {
+                    builder.Append(index);
+                }
+                builder.Append(' ');
+            }
+
+            return builder.ToString(0, builder.Length - 1);
+        }
+
+        private static int IntParseFast(string value)
+        {
+            var result = 0;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                result = 10 * result + (value[i] - 48);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Easy/FizzBuzz/FizzBuzz.UnitTests/UnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FizzBuzz.ConsoleApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void InputSample1ProducesOutputSample1()
+        {
+            //Arrange
+            var line = "3 5 10";
+            var expected = "1 2 F 4 B F 7 8 F B";
+
+            //Act
+            string result = FizzBuzzer.GenerateSequence(line);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void InputSample2ProducesOutputSample2()
+        {
+            //Arrange
+            var line = "2 7 15";
+            var expected = "1 F 3 F 5 F B F 9 F 11 F 13 FB 15";
+
+            //Act
+            string result = FizzBuzzer.GenerateSequence(line);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
