efa171415227cd5c8f4fa5f89a18fe2dfb097e14
James McDonnell <jjpmcd@gmail.com>
2015-12-18 22:32:07 +0000

    Imported previous RomanNumerals solution from Feb 2014. Refactored it to save 53248  bytes.

---
+++ Easy/RomanNumerals/RomanNumerals.ConsoleApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RomanNumerals.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(args[0]))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        Console.WriteLine(Converter.ToRomanNumerals(line));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Converter
+    {
+        public static string ToRomanNumerals(string decimalAsString)
+        {
+            var min = 'I';
+            var mid = 'V';
+            var max = 'X';
+            var builder = new StringBuilder();
+
+            for (var i = 1; i <= decimalAsString.Length; i++)
+            {
+                switch (i)
+                {
+                    case 2:
+                        min = 'X';
+                        mid = 'L';
+                        max = 'C';
+                        break;
+                    case 3:
+                        min = 'C';
+                        mid = 'D';
+                        max = 'M';
+                        break;
+                    case 4:
+                        min = 'M';
+                        break;
+                }
+
+                switch (decimalAsString[decimalAsString.Length - i])
+                {
+                    case '9':
+                        builder.Insert(0, max);
+                        builder.Insert(0, min);
+                        break;
+                    case '8':
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        builder.Insert(0, mid);
+                        break;
+                    case '7':
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        builder.Insert(0, mid);
+                        break;
+                    case '6':
+                        builder.Insert(0, min);
+                        builder.Insert(0, mid);
+                        break;
+                    case '5':
+                        builder.Insert(0, mid);
+                        break;
+                    case '4':
+                        builder.Insert(0, mid);
+                        builder.Insert(0, min);
+                        break;
+                    case '3':
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        break;
+                    case '2':
+                        builder.Insert(0, min);
+                        builder.Insert(0, min);
+                        break;
+                    case '1':
+                        builder.Insert(0, min);
+                        break;
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Easy/RomanNumerals/RomanNumerals.UnitTests/UnitTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals.ConsoleApp;
+
+namespace RomanNumerals.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string input = "159";
+            string expected = "CLIX";
+
+            //Act
+            string result = Converter.ToRomanNumerals(input);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string input = "296";
+            string expected = "CCXCVI";
+
+            //Act
+            string result = Converter.ToRomanNumerals(input);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string input = "3992";
+            string expected = "MMMCMXCII";
+
+            //Act
+            string result = Converter.ToRomanNumerals(input);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
