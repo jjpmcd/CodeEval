7557b2f599a7c5a8245d9eb99c337a72bee2bdf7
James McDonnell <jjpmcd@gmail.com>
2015-12-18 22:17:04 +0000

    Imported previous SumOfPrimes solution from June 2014. Refactored it to save 126976 bytes.

---
+++ Easy/SumOfPrimes/SumOfPrimes.ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfPrimes.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(PrimeProcessor.GetSumOfPrimes(1000));
+        }
+    }
+
+    public static class PrimeProcessor
+    {
+        public static int GetSumOfPrimes(int limit)
+        {
+            int candidate = 3;
+            int primesSum = 2;
+            int primesCount = 1;
+            while (primesCount < limit)
+            {
+                if (IsPrime(candidate))
+                {
+                    primesSum += candidate;
+                    primesCount++;
+                }
+                candidate += 2;
+            }
+            return primesSum;
+        }
+
+        public static bool IsPrime(int candidate)
+        {
+            if (candidate <= 3)
+            {
+                return candidate > 1;
+            }
+            if (candidate % 3 == 0 || candidate % 2 == 0)
+            {
+                return false;
+            }
+            for (var index = 5; index < (int)Math.Sqrt(candidate) + 1; index += 6)
+            {
+                if (candidate % index == 0 || candidate % (index + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Easy/SumOfPrimes/SumOfPrimes.UnitTests/UnitTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumOfPrimes.ConsoleApp;
+
+namespace SumOfPrimes.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void ReturnsFalseFor1()
+        {
+            //Arrange
+            const int candidate = 1;
+
+            //Act
+            bool result = PrimeProcessor.IsPrime(candidate);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void ReturnsTrueFor2()
+        {
+            //Arrange
+            const int candidate = 2;
+
+            //Act
+            bool result = PrimeProcessor.IsPrime(candidate);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void ReturnsTrueFor3()
+        {
+            //Arrange
+            const int candidate = 3;
+
+            //Act
+            bool result = PrimeProcessor.IsPrime(candidate);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void ReturnsFalseFor4()
+        {
+            //Arrange
+            const int candidate = 4;
+
+            //Act
+            bool result = PrimeProcessor.IsPrime(candidate);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void ReturnsTrueFor5()
+        {
+            //Arrange
+            const int candidate = 5;
+
+            //Act
+            bool result = PrimeProcessor.IsPrime(candidate);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SumOfFirst1000PrimeNumbersEquals3682913()
+        {
+            //Arrange
+            const int limit = 1000;
+            const int expected = 3682913;
+
+            //Act
+            int result = PrimeProcessor.GetSumOfPrimes(limit);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
