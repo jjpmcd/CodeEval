088caee82d93db7504207ac4353e3f602d845ea5
James McDonnell <jjpmcd@gmail.com>
2015-12-19 19:38:57 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/ArmstrongNumbers/ArmstrongNumbers.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ArmstrongNumbers.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(args[0]))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        // Do something with line
+                    }
+                }
+            }
+
+        }
+    }
+}
