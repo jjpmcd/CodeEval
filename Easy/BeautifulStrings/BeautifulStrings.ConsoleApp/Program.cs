ffb8309fa26e1e9848a4bd3b48874407df62cdbb
James McDonnell <jjpmcd@gmail.com>
2015-12-19 21:59:19 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/BeautifulStrings/BeautifulStrings.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BeautifulStrings.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(args[0]))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        // Do something with line                    }
+                    }
+                }
+            }
+        }
+    }
+}
