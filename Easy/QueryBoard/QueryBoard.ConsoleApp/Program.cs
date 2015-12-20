a8e3503f85acf06d5a8e637c3dac458da9cf8bed
James McDonnell <jjpmcd@gmail.com>
2015-12-20 13:45:18 +0000

    Implemented, tested and optimized Query method.

--- Easy/QueryBoard/QueryBoard.ConsoleApp/Program.cs
+++ Easy/QueryBoard/QueryBoard.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace QueryBoard.ConsoleApp
@@ -14,14 +12,105 @@ namespace QueryBoard.ConsoleApp
             {
                 using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
                 {
+                    var queryBoard = Board.GetNewBoard();
                     string line;
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        var result = Board.Query(line, queryBoard);
+                        if (result != -1) { Console.WriteLine(result); }
                     }
                 }
             }
+        }
+    }
+
+    public static class Board
+    {
+        public static int Query(string input, int[][] queryBoard)
+        {
+            var splitInput = input.Split(' ');
+            var target = IntParseFast(splitInput[1]);
+            int sum = -1;
+
+            if (input[3] == 'R')
+            {
+                SetRow(queryBoard, target, IntParseFast(splitInput[2]));
+            }
+            else if (input[3] == 'C')
+            {
+                SetCol(queryBoard, target, IntParseFast(splitInput[2]));
+            }
+            else if (input[5] == 'R')
+            {
+                sum = QueryRow(queryBoard, target);
+            }
+            else if (input[5] == 'C')
+            {
+                sum = QueryCol(queryBoard, target);
+            }
+
+            return sum;
+        }
+
+        private static int QueryCol(int[][] queryBoard, int target)
+        {
+            int sum = 0;
+            for (var i = 0; i < queryBoard.Length; i++)
+            {
+                sum += queryBoard[i][target];
+            }
+
+            return sum;
+        }
+
+        private static int QueryRow(int[][] queryBoard, int target)
+        {
+            int sum = 0;
+            for (var i = 0; i < queryBoard[target].Length; i++)
+            {
+                sum += queryBoard[target][i];
+            }
+
+            return sum;
+        }
+
+        private static void SetCol(int[][] queryBoard, int target, int value)
+        {
+            for (var i = 0; i < queryBoard.Length; i++)
+            {
+                queryBoard[i][target] = value;
+            }
+        }
+
+        private static void SetRow(int[][] queryBoard, int target, int value)
+        {
+            for (var i = 0; i < queryBoard[target].Length; i++)
+            {
+                queryBoard[target][i] = value;
+            }
+        }
+
+        public static int[][] GetNewBoard()
+        {
+            var queryBoard = new int[256][];
+            for (var i = 0; i < queryBoard.Length; i++)
+            {
+                queryBoard[i] = new int[256];
+            }
+            return queryBoard;
+        }
+
+        private static int IntParseFast(string value)
+        {
+            var result = 0;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                result = 10 * result + (value[i] - 48);
+            }
+
+            return result;
         }
     }
 }
--- Easy/QueryBoard/QueryBoard.UnitTests/UnitTests.cs
+++ Easy/QueryBoard/QueryBoard.UnitTests/UnitTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueryBoard.ConsoleApp;
 
 namespace QueryBoard.UnitTests
 {
@@ -9,6 +12,95 @@ namespace QueryBoard.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = @"SetCol 32 20
+SetRow 15 7
+SetRow 16 31
+QueryCol 32";
+            string expected = "5118";
+
+            string result = TestHelper(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = @"SetCol 32 20
+SetRow 15 7
+SetRow 16 31
+QueryCol 32
+SetCol 2 14
+QueryRow 10";
+            string expected = @"5118
+34";
+
+            string result = TestHelper(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = @"SetCol 32 20
+SetRow 15 7
+SetRow 16 31
+QueryCol 32
+SetCol 2 14
+QueryRow 10
+SetCol 14 0
+QueryRow 15";
+            string expected = @"5118
+34
+1792";
+
+            string result = TestHelper(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = @"SetCol 32 20
+SetRow 15 7
+SetRow 16 31
+QueryCol 32
+SetCol 2 14
+QueryRow 10
+SetCol 14 0
+QueryRow 15
+SetRow 10 1
+QueryCol 2";
+            string expected = @"5118
+34
+1792
+3571";
+
+            string result = TestHelper(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        public static string TestHelper(string lines)
+        {
+            var queryBoard = Board.GetNewBoard();
+            var builder = new StringBuilder();
+            var reader = new StringReader(lines);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var queryResult = Board.Query(line, queryBoard);
+                if (queryResult == -1) continue;
+                if (builder.Length != 0)
+                {
+                    builder.AppendLine();
+                }
+                builder.Append(queryResult);
+            }
+
+            return builder.ToString();
         }
     }
 }
