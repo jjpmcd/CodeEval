86fb7aa36cb5dee53e9f7a1b5f87e3852693a359
James McDonnell <jjpmcd@gmail.com>
2015-12-21 19:38:32 +0000

    Implemented and tested SimpleSort method using double parsing. Could be optimized by just sorting the elements as strings.

--- Easy/SimpleSorting/SimpleSorting.ConsoleApp/Program.cs
+++ Easy/SimpleSorting/SimpleSorting.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace SimpleSorting.ConsoleApp
@@ -18,10 +16,37 @@ namespace SimpleSorting.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(Sorter.SimpleSort(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Sorter
+    {
+        public static string SimpleSort(string input)
+        {
+            // TODO: Just sort the string values, don't parse
+            var splitInput = input.Split(' ');
+            var parsedInput = new double[splitInput.Length];
+
+            for (var i = 0; i < parsedInput.Length; i++)
+            {
+                parsedInput[i] = double.Parse(splitInput[i]);
+            }
+
+            Array.Sort(parsedInput);
+
+            var builder = new StringBuilder(parsedInput[0].ToString("F3"));
+
+            for (var i = 1; i < parsedInput.Length; i++)
+            {
+                builder.Append(' ');
+                builder.Append(parsedInput[i].ToString("F3"));
+            }
+
+            return builder.ToString();
         }
     }
 }
--- Easy/SimpleSorting/SimpleSorting.UnitTests/UnitTests.cs
+++ Easy/SimpleSorting/SimpleSorting.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSorting.ConsoleApp;
 
 namespace SimpleSorting.UnitTests
 {
@@ -9,6 +10,23 @@ namespace SimpleSorting.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "70.920 -38.797 14.354 99.323 90.374 7.581";
+            string expected = "-38.797 7.581 14.354 70.920 90.374 99.323";
+
+            string result = Sorter.SimpleSort(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "-37.507 -3.263 40.079 27.999 65.213 -55.552";
+            string expected = "-55.552 -37.507 -3.263 27.999 40.079 65.213";
+
+            string result = Sorter.SimpleSort(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
