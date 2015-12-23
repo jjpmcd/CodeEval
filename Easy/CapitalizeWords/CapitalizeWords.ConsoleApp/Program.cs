2dba7fb656f26a08eb1679b056f05fba703f4d95
James McDonnell <jjpmcd@gmail.com>
2015-12-23 23:26:15 +0000

    Implemented and tested ToTitleCase method.

--- Easy/CapitalizeWords/CapitalizeWords.ConsoleApp/Program.cs
+++ Easy/CapitalizeWords/CapitalizeWords.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace CapitalizeWords.ConsoleApp
@@ -18,10 +16,120 @@ namespace CapitalizeWords.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(Caser.ToTitleCase(line));               
                     }
                 }
             }
+        }
+    }
+
+    public static class Caser
+    {
+        public static string ToTitleCase(string input)
+        {
+            var output = "";
+            var capitilize = true;
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (capitilize)
+                {
+                    output += Capitalize(input[index]);
+                    capitilize = false;
+                    continue;
+                }
+
+                if (input[index] == ' ') { capitilize = true; }
+
+                output += input[index];
+            }
+
+            return output;
+        }
+
+        private static char Capitalize(char candidate)
+        {
+            switch (candidate)
+            {
+                case 'a':
+                    candidate = 'A';
+                    break;
+                case 'b':
+                    candidate = 'B';
+                    break;
+                case 'c':
+                    candidate = 'C';
+                    break;
+                case 'd':
+                    candidate = 'D';
+                    break;
+                case 'e':
+                    candidate = 'E';
+                    break;
+                case 'f':
+                    candidate = 'F';
+                    break;
+                case 'g':
+                    candidate = 'G';
+                    break;
+                case 'h':
+                    candidate = 'H';
+                    break;
+                case 'i':
+                    candidate = 'I';
+                    break;
+                case 'j':
+                    candidate = 'J';
+                    break;
+                case 'k':
+                    candidate = 'K';
+                    break;
+                case 'l':
+                    candidate = 'L';
+                    break;
+                case 'm':
+                    candidate = 'M';
+                    break;
+                case 'n':
+                    candidate = 'N';
+                    break;
+                case 'o':
+                    candidate = 'O';
+                    break;
+                case 'p':
+                    candidate = 'P';
+                    break;
+                case 'q':
+                    candidate = 'Q';
+                    break;
+                case 'r':
+                    candidate = 'R';
+                    break;
+                case 's':
+                    candidate = 'S';
+                    break;
+                case 't':
+                    candidate = 'T';
+                    break;
+                case 'u':
+                    candidate = 'U';
+                    break;
+                case 'v':
+                    candidate = 'V';
+                    break;
+                case 'w':
+                    candidate = 'W';
+                    break;
+                case 'x':
+                    candidate = 'X';
+                    break;
+                case 'y':
+                    candidate = 'Y';
+                    break;
+                case 'z':
+                    candidate = 'Z';
+                    break;
+            }
+            return candidate;
         }
     }
 }
--- Easy/CapitalizeWords/CapitalizeWords.UnitTests/UnitTests.cs
+++ Easy/CapitalizeWords/CapitalizeWords.UnitTests/UnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using CapitalizeWords.ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CapitalizeWords.UnitTests
@@ -9,6 +9,42 @@ namespace CapitalizeWords.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "Hello world";
+            string expected = "Hello World";
+
+            string result = Caser.ToTitleCase(input);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "javaScript language";
+            string expected = "JavaScript Language";
+
+            string result = Caser.ToTitleCase(input);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "a letter";
+            string expected = "A Letter";
+
+            string result = Caser.ToTitleCase(input);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = "1st thing";
+            string expected = "1st Thing";
+
+            string result = Caser.ToTitleCase(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
