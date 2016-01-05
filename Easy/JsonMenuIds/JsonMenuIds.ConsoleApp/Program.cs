9f394807ca393f2fe5fc0dd605c711c7a97d48dc
James McDonnell <jjpmcd@gmail.com>
2016-01-05 21:39:04 +0000

    Implemented, tested and optimized GetSum method.

--- Easy/JsonMenuIds/JsonMenuIds.ConsoleApp/Program.cs
+++ Easy/JsonMenuIds/JsonMenuIds.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace JsonMenuIds.ConsoleApp
@@ -18,10 +16,56 @@ namespace JsonMenuIds.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(JsonParser.GetSum(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class JsonParser
+    {
+        public static int GetSum(string input)
+        {
+            char[] prefix = { 'L', 'a', 'b', 'e', 'l', ' ' };
+            var sum = 0;
+            var matched = 0;
+            var startIndex = 0;
+            var length = 0;
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (matched == prefix.Length)
+                {
+                    if (length == 0) startIndex = index;
+
+                    if (char.IsDigit(input[index]))
+                    {
+                        length++;
+                        continue;
+                    }
+
+                    sum += IntParseFast(input.Substring(startIndex, length));
+                    length = 0;
+                    matched = 0;
+                }
+
+                if (input[index] == prefix[matched]) matched++;
+            }
+
+            return sum;
+        }
+
+        private static int IntParseFast(string value)
+        {
+            var result = 0;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                result = 10 * result + (value[i] - 48);
+            }
+
+            return result;
         }
     }
 }
--- Easy/JsonMenuIds/JsonMenuIds.UnitTests/UnitTests.cs
+++ Easy/JsonMenuIds/JsonMenuIds.UnitTests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JsonMenuIds.ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JsonMenuIds.UnitTests
@@ -9,6 +10,35 @@ namespace JsonMenuIds.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input =
+                "{\"menu\": {\"header\": \"menu\", \"items\": [{\"id\": 27}, {\"id\": 0, \"label\": \"Label 0\"}, null, {\"id\": 93}, {\"id\": 85}, {\"id\": 54}, null, {\"id\": 46, \"label\": \"Label 46\"}]}}";
+            int expected = 46;
+
+            int result = JsonParser.GetSum(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "{\"menu\": {\"header\": \"menu\", \"items\": [{\"id\": 81}]}}";
+            int expected = 0;
+
+            int result = JsonParser.GetSum(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "{\"menu\": {\"header\": \"menu\", \"items\": [{\"id\": 70, \"label\": \"Label 70\"}, {\"id\": 85, \"label\": \"Label 85\"}, {\"id\": 93, \"label\": \"Label 93\"}, {\"id\": 2}]}}";
+            int expected = 248;
+
+            int result = JsonParser.GetSum(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
