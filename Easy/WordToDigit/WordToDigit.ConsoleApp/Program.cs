508391da24446f3a8b4e108e110951c334515f06
James McDonnell <jjpmcd@gmail.com>
2016-01-08 20:18:02 +0000

    Implemented, tested and optimized GetDigits method.

--- Easy/WordToDigit/WordToDigit.ConsoleApp/Program.cs
+++ Easy/WordToDigit/WordToDigit.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace WordToDigit.ConsoleApp
@@ -18,10 +16,89 @@ namespace WordToDigit.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(Parser.GetDigits(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Parser
+    {
+        public static string GetDigits(string input)
+        {
+            var output = "";
+            var skip = false;
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (skip)
+                {
+                    if (input[index] == ';')
+                    {
+                        skip = false;
+                    }
+                    continue;
+                }
+                if (input[index] == 't')
+                {
+                    if (input[index + 1] == 'w')
+                    {
+                        output += '2';
+                    }
+                    else
+                    {
+                        output += '3';
+                    }
+                    skip = true;
+                }
+                else if (input[index] == 'f')
+                {
+                    if (input[index + 1] == 'o')
+                    {
+                        output += '4';
+                    }
+                    else
+                    {
+                        output += '5';
+                    }
+                    skip = true;
+                }
+                else if (input[index] == 's')
+                {
+                    if (input[index + 1] == 'i')
+                    {
+                        output += '6';
+                    }
+                    else
+                    {
+                        output += '7';
+                    }
+                    skip = true;
+                }
+                else if (input[index] == 'e')
+                {
+                    output += '8';
+                    skip = true;
+                }
+                else if (input[index] == 'n')
+                {
+                    output += '9';
+                    skip = true;
+                }
+                else if (input[index] == 'z')
+                {
+                    output += '0';
+                    skip = true;
+                }
+                else if (input[index] == 'o')
+                {
+                    output += '1';
+                    skip = true;
+                }
+            }
+
+            return output;
         }
     }
 }
--- Easy/WordToDigit/WordToDigit.UnitTests/UnitTests.cs
+++ Easy/WordToDigit/WordToDigit.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordToDigit.ConsoleApp;
 
 namespace WordToDigit.UnitTests
 {
@@ -9,6 +10,23 @@ namespace WordToDigit.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "zero;two;five;seven;eight;four";
+            string expected = "025784";
+
+            string result = Parser.GetDigits(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "three;seven;eight;nine;two";
+            string expected = "37892";
+
+            string result = Parser.GetDigits(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
