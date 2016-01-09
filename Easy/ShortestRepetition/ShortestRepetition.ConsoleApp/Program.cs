8e38cead8a4705a61ff85472307a63693b405bd7
James McDonnell <jjpmcd@gmail.com>
2016-01-09 19:27:51 +0000

    Implemented and tested GetSmallestPeriod method.

--- Easy/ShortestRepetition/ShortestRepetition.ConsoleApp/Program.cs
+++ Easy/ShortestRepetition/ShortestRepetition.ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 
 namespace ShortestRepetition.ConsoleApp
@@ -9,6 +8,30 @@ namespace ShortestRepetition.ConsoleApp
     {
         private static void Main(string[] args)
         {
+            using (var fileStream = File.OpenRead(args[0]))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.ASCII, false))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        Console.WriteLine(Scanner.GetSmallestPeriod(line));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Scanner
+    {
+        public static int GetSmallestPeriod(string input)
+        {
+            for (var index = 1; index < input.Length; index++)
+            {
+                if (input[0] == input[index]) return index;
+            }
+            return input.Length;
         }
     }
 }
--- Easy/ShortestRepetition/ShortestRepetition.UnitTests/UnitTests.cs
+++ Easy/ShortestRepetition/ShortestRepetition.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShortestRepetition.ConsoleApp;
 
 namespace ShortestRepetition.UnitTests
 {
@@ -9,6 +10,45 @@ namespace ShortestRepetition.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "abcabcabcabc";
+            int expected = 3;
+
+            int result = Scanner.GetSmallestPeriod(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "bcbcbcbcbcbcbcbcbcbcbcbcbcbc";
+            int expected = 2;
+
+            int result = Scanner.GetSmallestPeriod(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "dddddddddddddddddddd";
+            int expected = 1;
+
+            int result = Scanner.GetSmallestPeriod(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = "adcdefg";
+            int expected = 7;
+
+            int result = Scanner.GetSmallestPeriod(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
