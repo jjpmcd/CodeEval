62aa08eec436be83dac690cde614ce92dcfd2a05
James McDonnell <jjpmcd@gmail.com>
2016-01-09 19:08:00 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/ShortestRepetition/ShortestRepetition.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShortestRepetition.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
