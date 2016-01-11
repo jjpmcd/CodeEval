012cf21c0df4a0c1336e1e2650c7a4f4409df46b
James McDonnell <jjpmcd@gmail.com>
2016-01-11 17:35:46 +0000

    Implemented and tested GetSwapped method.

--- Easy/SwapElements/SwapElements.ConsoleApp/Program.cs
+++ Easy/SwapElements/SwapElements.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace SwapElements.ConsoleApp
@@ -18,10 +16,82 @@ namespace SwapElements.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(Swapper.GetSwapped(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Swapper
+    {
+        public static string GetSwapped(string input)
+        {
+            var indexOfColon = input.IndexOf(':');
+            var elements = input.Substring(0, indexOfColon - 1).Split(' ');
+
+            ParseInstructions(input, indexOfColon, elements);
+
+            return string.Join(" ", elements);
+        }
+
+        private static void ParseInstructions(string input, int indexOfColon, string[] elements)
+        {
+            var index = indexOfColon + 2;
+            var firstIndex = 0;
+            var secondIndex = 0;
+            var first = true;
+
+            while (index < input.Length)
+            {
+                if (input[index] == '-')
+                {
+                    first = false;
+                    index++;
+                    continue;
+                }
+
+                if (input[index] == ',')
+                {
+                    Swap(elements, firstIndex, secondIndex);
+
+                    first = true;
+                    firstIndex = 0;
+                    secondIndex = 0;
+                    index += 2;
+                    continue;
+                }
+
+                if (first)
+                {
+                    firstIndex = CumulativeIntFastParse(input[index], firstIndex);
+                }
+                else
+                {
+                    secondIndex = CumulativeIntFastParse(input[index], secondIndex);
+
+                    if (index == input.Length - 1)
+                    {
+                        Swap(elements, firstIndex, secondIndex);
+                        break;
+                    }
+                }
+
+                index++;
+            }
+        }
+
+        private static int CumulativeIntFastParse(char c, int total)
+        {
+            total = 10 * total + (c - 48);
+            return total;
+        }
+
+        private static void Swap(string[] elements, int firstIndex, int secondIndex)
+        {
+            var temp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = temp;
         }
     }
 }
--- Easy/SwapElements/SwapElements.UnitTests/UnitTests.cs
+++ Easy/SwapElements/SwapElements.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwapElements.ConsoleApp;
 
 namespace SwapElements.UnitTests
 {
@@ -9,6 +10,23 @@ namespace SwapElements.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "1 2 3 4 5 6 7 8 9 : 0-8";
+            string expected = "9 2 3 4 5 6 7 8 1";
+
+            string result = Swapper.GetSwapped(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "1 2 3 4 5 6 7 8 9 10 : 0-1, 1-3";
+            string expected = "2 4 3 1 5 6 7 8 9 10";
+
+            string result = Swapper.GetSwapped(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
