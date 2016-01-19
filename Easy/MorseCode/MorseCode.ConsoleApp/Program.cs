18870c4b06164c10df49c96ba40c96f15f8a669c
James McDonnell <jjpmcd@gmail.com>
2016-01-19 11:11:30 +0000

    Implemented and tested GetDecodedMessage method. Could be optimized using a dichotomic search.

--- Easy/MorseCode/MorseCode.ConsoleApp/Program.cs
+++ Easy/MorseCode/MorseCode.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace MorseCode.ConsoleApp
@@ -18,10 +16,132 @@ namespace MorseCode.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // Do something with line                   
+                        Console.WriteLine(Morser.GetDecodedMessage(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Morser
+    {
+        public static string GetDecodedMessage(string input)
+        {
+            var output = "";
+            var startIndex = 0;
+            var length = 0;
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (input[index] != ' ')
+                {
+                    length++;
+                    continue;
+                }
+                output += GetDecodeCharacter(input.Substring(startIndex, length));
+
+                if (input[index + 1] == ' ')
+                {
+                    index++;
+                    output += ' ';
+                }
+
+                startIndex = index + 1;
+                length = 0;
+            }
+
+            output += GetDecodeCharacter(input.Substring(startIndex));
+
+            return output;
+        }
+
+        private static char GetDecodeCharacter(string input)
+        {
+            if (input.Length != 5)
+            {
+                switch (input)
+                {
+                    case ".-":
+                        return 'A';
+                    case "-...":
+                        return 'B';
+                    case "-.-.":
+                        return 'C';
+                    case "-..":
+                        return 'D';
+                    case ".":
+                        return 'E';
+                    case "..-.":
+                        return 'F';
+                    case "--.":
+                        return 'G';
+                    case "....":
+                        return 'H';
+                    case "..":
+                        return 'I';
+                    case ".---":
+                        return 'J';
+                    case "-.-":
+                        return 'K';
+                    case ".-..":
+                        return 'L';
+                    case "--":
+                        return 'M';
+                    case "-.":
+                        return 'N';
+                    case "---":
+                        return 'O';
+                    case ".--.":
+                        return 'P';
+                    case "--.-":
+                        return 'Q';
+                    case ".-.":
+                        return 'R';
+                    case "...":
+                        return 'S';
+                    case "-":
+                        return 'T';
+                    case "..-":
+                        return 'U';
+                    case "...-":
+                        return 'V';
+                    case ".--":
+                        return 'W';
+                    case "-..-":
+                        return 'X';
+                    case "-.--":
+                        return 'Y';
+                    case "--..":
+                        return 'Z';
+                }
+            }
+            else
+            {
+                switch (input)
+                {
+                    case ".----":
+                        return '1';
+                    case "..---":
+                        return '2';
+                    case "...--":
+                        return '3';
+                    case "....-":
+                        return '4';
+                    case ".....":
+                        return '5';
+                    case "-....":
+                        return '6';
+                    case "--...":
+                        return '7';
+                    case "---..":
+                        return '8';
+                    case "----.":
+                        return '9';
+                    case "-----":
+                        return '0';
+                }
+            }
+            throw new ArgumentException("Unexpected Morse Code.");
         }
     }
 }
--- Easy/MorseCode/MorseCode.UnitTests/UnitTests.cs
+++ Easy/MorseCode/MorseCode.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MorseCode.ConsoleApp;
 
 namespace MorseCode.UnitTests
 {
@@ -9,6 +10,23 @@ namespace MorseCode.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = ".- ...- ..--- .-- .... .. . -.-. -..-  ....- .....";
+            string expected = "AV2WHIECX 45";
+
+            string result = Morser.GetDecodedMessage(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "-... .... ...--";
+            string expected = "BH3";
+
+            string result = Morser.GetDecodedMessage(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
