8d2fb16aeb7be3bba1c38edfec67f7a5af55fdd8
James McDonnell <jjpmcd@gmail.com>
2016-01-21 10:21:55 +0000

    Created solution with console application and unit test projects in Visual Studio 2015.

---
+++ Easy/RoadTrip/RoadTrip.UnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadTrip.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
