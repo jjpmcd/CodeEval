65353f0694e132ea98fa133cb2cd0d383f8100ea
James McDonnell <jjpmcd@gmail.com>
2016-01-27 18:58:30 +0000

    Implemented and tested GetNextManoeuvre method.

--- Easy/RacingChars/RacingChars.ConsoleApp/Program.cs
+++ Easy/RacingChars/RacingChars.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace RacingChars.ConsoleApp
@@ -18,10 +16,69 @@ namespace RacingChars.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // TODO: Do something with line
+                        Console.WriteLine(Racer.GetNextManoeuvre(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Racer
+    {
+        public static int IndexOfPreviousManoeuvre = -1;
+        private static readonly char[] Manoeuvres = { '/', '|', '\\' };
+
+        public static string GetNextManoeuvre(string input)
+        {
+            var indexOfNextManoeuvre = GetIndexOfNextManoeuvre(input);
+            var output = GetOutputString(input, indexOfNextManoeuvre);
+            IndexOfPreviousManoeuvre = indexOfNextManoeuvre;
+            return output;
+        }
+
+        private static string GetOutputString(string input, int indexOfNextManoeuvre)
+        {
+            var output = new char[input.Length];
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (index == indexOfNextManoeuvre)
+                {
+                    if (IndexOfPreviousManoeuvre == -1)
+                    {
+                        output[index] = Manoeuvres[1];
+                        continue;
+                    }
+
+                    output[index] = Manoeuvres[1 + indexOfNextManoeuvre - IndexOfPreviousManoeuvre];
+                    continue;
+                }
+
+                output[index] = input[index];
+            }
+
+            return new string(output);
+        }
+
+        private static int GetIndexOfNextManoeuvre(string input)
+        {
+            if (IndexOfPreviousManoeuvre == -1) return input.IndexOf('_');
+
+            var indexOfBestManoeuvre = -1;
+
+            for (var index = Math.Max(0, IndexOfPreviousManoeuvre - 1);
+                index < Math.Min(IndexOfPreviousManoeuvre + 2, input.Length);
+                index++)
+            {
+                if (input[index] == 'C')
+                {
+                    indexOfBestManoeuvre = index;
+                    break;
+                }
+
+                if (input[index] == '_') indexOfBestManoeuvre = index;
+            }
+
+            return indexOfBestManoeuvre;
         }
     }
 }
--- Easy/RacingChars/RacingChars.UnitTests/UnitTests.cs
+++ Easy/RacingChars/RacingChars.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RacingChars.ConsoleApp;
 
 namespace RacingChars.UnitTests
 {
@@ -9,6 +10,120 @@ namespace RacingChars.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input =      "#########_##";
+            string expected =   "#########|##";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Racer.IndexOfPreviousManoeuvre = 9;
+            string input =      "########C_##";
+            string expected =   "########/_##";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Racer.IndexOfPreviousManoeuvre = 8;
+            string input =      "#######_####";
+            string expected =   "#######/####";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Racer.IndexOfPreviousManoeuvre = 7;
+            string input =      "######_#C###";
+            string expected =  @"######_#\###";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Racer.IndexOfPreviousManoeuvre = 8;
+            string input =      "#######_C###";
+            string expected =   "#######_|###";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Racer.IndexOfPreviousManoeuvre = 8;
+            string input =      "#######_####";
+            string expected =   "#######/####";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Racer.IndexOfPreviousManoeuvre = 7;
+            string input =      "######C#_###";
+            string expected =   "######/#_###";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Racer.IndexOfPreviousManoeuvre = 6;
+            string input =      "######C_####";
+            string expected =   "######|_####";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Racer.IndexOfPreviousManoeuvre = 6;
+            string input =      "#######_####";
+            string expected =  @"#######\####";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Racer.IndexOfPreviousManoeuvre = 7;
+            string input =      "#######_####";
+            string expected =   "#######|####";
+
+            string result = Racer.GetNextManoeuvre(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
