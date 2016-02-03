b77131b9fe8c4eed609d638d6e01ddf8fedd21b7
James McDonnell <jjpmcd@gmail.com>
2016-02-03 17:53:28 +0000

    Implemented and tested GetActualWorkExperience method.

--- Easy/WorkingExperience/WorkingExperience.ConsoleApp/Program.cs
+++ Easy/WorkingExperience/WorkingExperience.ConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace WorkingExperience.ConsoleApp
@@ -18,9 +17,112 @@ namespace WorkingExperience.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // TODO: Do something with line
+                        Console.WriteLine(Resumer.GetActualWorkExperience(line));
                     }
                 }
+            }
+        }
+    }
+
+    public static class Resumer
+    {
+        public static int GetActualWorkExperience(string input)
+        {
+            var intervals = ParseIntervals(input);
+
+            for (var i = 0; i < intervals.Count; i++)
+            {
+                for (var j = i + 1; j < intervals.Count; j++)
+                {
+                    if (intervals[i][1] < intervals[j][0]) continue;
+                    if (intervals[i][0] > intervals[j][1]) continue;
+                    intervals[j][0] = Math.Min(intervals[i][0], intervals[j][0]);
+                    intervals[j][1] = Math.Max(intervals[i][1], intervals[j][1]);
+                    intervals.RemoveAt(i);
+                    i--;
+                    break;
+                }
+            }
+
+            var totalMonths = 0;
+            for (var index = 0; index < intervals.Count; index++)
+                totalMonths += intervals[index][1] - intervals[index][0];
+
+            return totalMonths / 12;
+        }
+
+        private static List<int[]> ParseIntervals(string input)
+        {
+            var months = 0;
+            var interval = new int[2];
+            var intervals = new List<int[]>();
+            for (var index = 0; index < input.Length; index++)
+            {
+                switch (input[index])
+                {
+                    case ' ':
+                        months += ParseMonth(input, index);
+                        continue;
+                    case '-':
+                        months += ParseYearToMonths(input, index);
+                        interval[0] = months;
+                        months = 0;
+                        continue;
+                    case ';':
+                        months += ParseYearToMonths(input, index) + 1;
+                        interval[1] = months;
+                        intervals.Add(interval);
+                        interval = new int[2];
+                        months = 0;
+                        index++;
+                        break;
+                }
+            }
+
+            months += ParseYearToMonths(input, input.Length) + 1;
+            interval[1] = months;
+            intervals.Add(interval);
+
+            return intervals;
+        }
+
+        private static int ParseYearToMonths(string input, int index)
+        {
+            var result = 0;
+
+            for (var i = index - 4; i < index; i++)
+            {
+                result = 10 * result + (input[i] - 48);
+            }
+            result -= 1990;
+            result *= 12;
+
+            return result;
+        }
+
+        private static int ParseMonth(string input, int index)
+        {
+            switch (input[index - 3])
+            {
+                case 'J':
+                    if (input[index - 2] == 'a') return 1;
+                    return input[index - 1] == 'n' ? 6 : 7;
+                case 'M':
+                    return input[index - 1] == 'r' ? 3 : 5;
+                case 'A':
+                    return input[index - 2] == 'p' ? 4 : 8;
+                case 'F':
+                    return 2;
+                case 'S':
+                    return 9;
+                case 'O':
+                    return 10;
+                case 'N':
+                    return 11;
+                case 'D':
+                    return 12;
+                default:
+                    return 0;
             }
         }
     }
--- Easy/WorkingExperience/WorkingExperience.UnitTests/UnitTests.cs
+++ Easy/WorkingExperience/WorkingExperience.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkingExperience.ConsoleApp;
 
 namespace WorkingExperience.UnitTests
 {
@@ -9,6 +10,67 @@ namespace WorkingExperience.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "Feb 2004-Dec 2009; Sep 2004-Jul 2008";
+            int expected = 5;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "Aug 2013-Mar 2014; Apr 2013-Aug 2013; Jun 2014-Aug 2015; Apr 2003-Nov 2004; Apr 2014-Jan 2015";
+            int expected = 4;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "Mar 2003-Jul 2003; Nov 2003-Jan 2004; Apr 1999-Nov 1999";
+            int expected = 1;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = "Apr 1992-Dec 1993; Feb 1996-Sep 1997; Jan 2002-Jun 2002; Sep 2003-Apr 2004; Feb 2010-Nov 2011";
+            int expected = 6;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string input = "Feb 2004-May 2004; Jun 2004-Jul 2004";
+            int expected = 0;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string input = "Dec 2003-Nov 2004; Apr 2008-May 2008; Apr 1996-Apr 1997; Nov 2006-Feb 2007; Aug 2008-Dec 2009; Jul 1991-Dec 1992; Sep 1998-Apr 2000; Dec 1997-Jun 1999; Jan 2006-Sep 2007; Feb 2010-Nov 2010; Aug 1996-Apr 1998; Oct 1990-Mar 1992; Jun 1990-Nov 1990; Jul 2009-Feb 2011; Dec 1998-Jan 2000; Apr 1995-Jul 1996";
+            int expected = 13;
+
+            int result = Resumer.GetActualWorkExperience(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
