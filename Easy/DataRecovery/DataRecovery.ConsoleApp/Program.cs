3043bbe7c1e8de23eed208da1fd907188d6b8ebf
James McDonnell <jjpmcd@gmail.com>
2016-02-09 12:50:58 +0000

    Implemented and tested GetSentence method.

--- Easy/DataRecovery/DataRecovery.ConsoleApp/Program.cs
+++ Easy/DataRecovery/DataRecovery.ConsoleApp/Program.cs
@@ -18,10 +18,55 @@ namespace DataRecovery.ConsoleApp
                     while (!reader.EndOfStream)
                     {
                         line = reader.ReadLine();
-                        // TODO: Do something with line
+                        Console.WriteLine(Reconstructor.GetSentence(line));
                     }
                 }
             }
+        }
+    }
+
+    public static class Reconstructor
+    {
+        public static string GetSentence(string input)
+        {
+            var indexOfSemiColon = input.IndexOf(';');
+            var words = input.Substring(0, indexOfSemiColon).Split(' ');
+            var indices = GetIndices(input.Substring(indexOfSemiColon + 1, input.Length - indexOfSemiColon - 1));
+
+            var output = words[indices.IndexOf(1)];
+            for (var position = 2; position < words.Length + 1; position++)
+            {
+                output += ' ';
+                output += words[indices.IndexOf(position)];
+            }
+
+            return output;
+        }
+
+        private static List<int> GetIndices(string indicesString)
+        {
+            var indices = new List<int>();
+            var runningParse = 0;
+            var sum = 0;
+            for (var index = 0; index < indicesString.Length; index++)
+            {
+                if (indicesString[index] == ' ')
+                {
+                    indices.Add(runningParse);
+                    sum += runningParse;
+                    runningParse = 0;
+                    continue;
+                }
+                runningParse = 10 * runningParse + (indicesString[index] - 48);
+            }
+            indices.Add(runningParse);
+            sum += runningParse;
+
+            var length = indices.Count + 1;
+            var missingIndex = length * (length + 1) / 2 - sum;
+
+            indices.Add(missingIndex);
+            return indices;
         }
     }
 }
--- Easy/DataRecovery/DataRecovery.UnitTests/UnitTests.cs
+++ Easy/DataRecovery/DataRecovery.UnitTests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataRecovery.ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataRecovery.UnitTests
@@ -9,6 +10,34 @@ namespace DataRecovery.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            string input = "2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2";
+            string expected = "However, it was not implemented until 1998 and 2000";
+
+            string result = Reconstructor.GetSentence(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "programming first The language;3 2 1";
+            string expected = "The first programming language";
+
+            string result = Reconstructor.GetSentence(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9";
+            string expected = "The Manchester Mark 1 ran programs written in Autocode from 1952";
+
+            string result = Reconstructor.GetSentence(input);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
